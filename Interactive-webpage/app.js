561162be90c8e094930271ed445a746f175dcb24
Ataullah Arabzai <ataullahforpc@gmail.com>
2026-06-10 11:12:14 +0430

    basic auth added

---
+++ Interactive-webpage/app.js
@@ -0,0 +1,82 @@
+let themeBtn = document.querySelector('#toggle-theme')
+let logOutBtn = document.querySelector('#logout-btn')
+let loginFormBtn = document.querySelector('#login-btn')
+
+let username = document.querySelector('#username')
+let userEmail = document.querySelector('#userEmail')
+let userPassword = document.querySelector('#userPassword')
+
+let loginForm = document.querySelector('.login-card')
+let dashboardView = document.querySelector('#dashboard')
+let navbarItems = document.querySelector('.nav-items')
+let messageCard = document.querySelector('#message-card')
+
+const updateUI = () => {
+    const user = localStorage.getItem('user')
+
+    if(user) {
+        loginForm.style.display = 'none'
+        dashboardView.style.display = 'block'
+        logOutBtn.style.display = 'block'
+
+    } else {
+        loginForm.style.display = 'flex'
+        dashboardView.style.display = 'none'
+        logOutBtn.style.display = 'none'
+        navbarItems.style.display = 'none'
+    }
+}
+
+loginFormBtn.addEventListener('click', () => {
+    if (username.value === "") {
+        messageCard.textContent = 'Plaese enter username'
+        messageCard.style.backgroundColor = '#bb2124'
+        messageCard.classList.add('show')
+        return;
+    }
+    if (userEmail.value === "") {
+        messageCard.textContent = 'Plaese enter Email'
+        messageCard.style.backgroundColor = '#bb2124'
+        messageCard.classList.add('show')
+        return;
+    }
+    if (userPassword.value === "") {
+        messageCard.textContent = 'Plaese enter Password'
+        messageCard.style.backgroundColor = '#bb2124'
+        messageCard.classList.add('show')
+        return;
+    }
+    
+    localStorage.setItem('user', JSON.stringify({
+        username: username.value,
+        userEmail: userEmail.value,
+        userPassword: userPassword.value
+    }))
+    
+    username.value = ''
+    userEmail.value = ''
+    userPassword.value = ''
+
+    messageCard.style.backgroundColor = '#22bb33'
+    messageCard.textContent = 'Login successful !'
+    messageCard.classList.add('show')
+
+    setTimeout(() => {
+        messageCard.style.display = 'none'
+        navbarItems.style.display = 'flex'
+    }, 1500);
+    
+    updateUI()
+})
+
+logOutBtn.addEventListener('click', () => {
+    localStorage.removeItem('user')
+
+    username.value = "";
+    userEmail.value = "";
+    userPassword.value = "";
+
+    updateUI()
+})
+
+updateUI()
